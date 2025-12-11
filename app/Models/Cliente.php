7e6145abd4c4e37a933e8747b7aefdbb6cc3e74c
Pedro Lucas <plfm3k@gmail.com>
2025-12-10 21:51:30 -0400

    feat: adiciona estrutura inicial do banco de dados e models da API

---
+++ app/Models/Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['telefone', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // public function compras(): HasMany
+    // {
+    //     return $this->hasMany(Compra::class); 
+    // }
+}
---
+++ app/Models/Produto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nome', 'preco', 'descricao', 'imagem', 'material', 'marca', 'categoria_id'];
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function itensCompra(): HasMany
+    {
+        return $this->hasMany(ItensCompra::class);
+    }
+
+}
