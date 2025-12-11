2d61bc338791de19df22ede810524f2d20170c50
Pedro Lucas <plfm3k@gmail.com>
2025-12-10 21:57:57 -0400

    feat: adiciona método isAdmin no model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Cliente::class);
     }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
